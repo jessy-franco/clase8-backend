33d80fc2d9141b6998be761717637961c85b168e
JessyFranco <jesicafranco_1518@outlook.es>
2024-01-23 11:22:59 -0300

    actualizacion app.js

--- src/app.js
+++ src/app.js
@@ -17,6 +17,16 @@ const routerPets= routerPet;
 app.use(express.json());
 app.use("/static",express.static(__dirname + "/public"))
 
+/* permite un registro de lo que genera la app en consolao en texto, para saber que esta pasando con el programa...buscar midleware "midleware de login/moro"*/
+
+/* si me llega un request pasa por este midleware y me manda esta info de la interaccion del usuario con el sistema */
+app.use((req, res, next)=>{
+    console.log(Date.now() + req.method + "-"+ req.ip + "-" + req.url);
+    next();
+})
+
+/* configurar el multer...leer la documentacion, es un midleware, añade un body object con los archivos que me lee el request, configurar para que todos los archivos que se carguen se configuren en la carpeta del proyecto que queramos*/
+
 /* routers */
 app.use("/api/users/" ,routerUsers);
 app.use("/api/pets/" ,routerPets);
